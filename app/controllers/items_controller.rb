480c1e3f0f395798324413c887bd2f21667ec040
Emma Nyakio Gacoki <emmanyakio@gmail.com>
2022-09-29 10:30:57 +0300

    commit tree 475cc0602fe05f96bda0ded911e53703fb841c06

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -1,2 +1,18 @@
 class ItemsController < ApplicationController
+
+    # rescue_from ActiveRecord::RecordNotFound, with: :render_not_found_response
+
+    def index
+        items = Item.all
+        if items
+        render json: items, include: :user
+        else
+            render json: {error: "Item not found"}, status: :not_found
+        end
+    end
+
+    # private
+    # def render_not_found_response
+    #     render json: {error: "Item not found"}, status: :not_found
+    # end
 end
